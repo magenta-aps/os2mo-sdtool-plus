28b0436e28224a5a39f175e66c2bdba21955b67e
Mads Riis-Weitling <mrw@magenta.dk>
2023-07-24 11:32:20 +0200

    chore: [#55848] Convert SD mock tree to `pytest` fixture

--- sdtoolplus/tests/conftest.py
+++ sdtoolplus/tests/conftest.py
@@ -6,11 +6,20 @@ from itertools import chain
 
 import pytest
 from graphql.language.ast import DocumentNode
+from sdclient.responses import GetDepartmentResponse
+from sdclient.responses import GetOrganizationResponse
 
 from ..mo_org_unit_importer import OrgUnit
 from ..mo_org_unit_importer import OrgUnitNode
 from ..mo_org_unit_importer import OrgUnitUUID
 from ..mo_org_unit_importer import OrgUUID
+
+
+class SharedIdentifier:
+    root_org_uuid: OrgUUID = uuid.uuid4()
+    child_org_unit_uuid: OrgUnitUUID = uuid.uuid4()
+    grandchild_org_unit_uuid: OrgUnitUUID = uuid.uuid4()
+    removed_org_unit_uuid: OrgUnitUUID = uuid.uuid4()
 
 
 class _MockGraphQLSession:
@@ -78,3 +87,149 @@ class _MockGraphQLSession:
 @pytest.fixture()
 def mock_graphql_session() -> _MockGraphQLSession:
     return _MockGraphQLSession()
+
+
+@pytest.fixture()
+def mock_sd_get_organization_response() -> GetOrganizationResponse:
+    sd_org_json = {
+        "RegionIdentifier": "RI",
+        "InstitutionIdentifier": "II",
+        "InstitutionUUIDIdentifier": str(SharedIdentifier.root_org_uuid),
+        "DepartmentStructureName": "Dep structure name",
+        "OrganizationStructure": {
+            "DepartmentLevelIdentifier": "Afdelings-niveau",
+            "DepartmentLevelReference": {
+                "DepartmentLevelIdentifier": "NY0-niveau",
+                "DepartmentLevelReference": {
+                    "DepartmentLevelIdentifier": "NY1-niveau"
+                }
+            }
+        },
+        "Organization": [
+            {
+                "ActivationDate": "1999-01-01",
+                "DeactivationDate": "2000-01-01",
+                "DepartmentReference": [
+                    {
+                        "DepartmentIdentifier": "Afd",
+                        "DepartmentUUIDIdentifier": "30000000-0000-0000-0000-000000000000",
+                        "DepartmentLevelIdentifier": "Afdelings-niveau",
+                        "DepartmentReference": [
+                            {
+                                "DepartmentIdentifier": "NY0",
+                                "DepartmentUUIDIdentifier": str(SharedIdentifier.grandchild_org_unit_uuid),
+                                "DepartmentLevelIdentifier": "NY0-niveau",
+                                "DepartmentReference": [
+                                    {
+                                        "DepartmentIdentifier": "NY1",
+                                        "DepartmentUUIDIdentifier": str(SharedIdentifier.child_org_unit_uuid),
+                                        "DepartmentLevelIdentifier": "NY1-niveau",
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "DepartmentIdentifier": "Afd",
+                        "DepartmentUUIDIdentifier": "40000000-0000-0000-0000-000000000000",
+                        "DepartmentLevelIdentifier": "Afdelings-niveau",
+                        "DepartmentReference": [
+                            {
+                                "DepartmentIdentifier": "NY0",
+                                "DepartmentUUIDIdentifier": str(SharedIdentifier.grandchild_org_unit_uuid),
+                                "DepartmentLevelIdentifier": "NY0-niveau",
+                                "DepartmentReference": [
+                                    {
+                                        "DepartmentIdentifier": "NY1",
+                                        "DepartmentUUIDIdentifier": str(SharedIdentifier.child_org_unit_uuid),
+                                        "DepartmentLevelIdentifier": "NY1-niveau",
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "DepartmentIdentifier": "Afd",
+                        "DepartmentUUIDIdentifier": "60000000-0000-0000-0000-000000000000",
+                        "DepartmentLevelIdentifier": "Afdelings-niveau",
+                        "DepartmentReference": [
+                            {
+                                "DepartmentIdentifier": "NY0",
+                                "DepartmentUUIDIdentifier": "50000000-0000-0000-0000-000000000000",
+                                "DepartmentLevelIdentifier": "NY0-niveau",
+                                "DepartmentReference": [
+                                    {
+                                        "DepartmentIdentifier": "NY1",
+                                        "DepartmentUUIDIdentifier": str(SharedIdentifier.child_org_unit_uuid),
+                                        "DepartmentLevelIdentifier": "NY1-niveau",
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                ]
+            }
+        ]
+    }
+    sd_org = GetOrganizationResponse.parse_obj(sd_org_json)
+    return sd_org
+
+
+@pytest.fixture()
+def mock_sd_get_department_response() -> GetDepartmentResponse:
+    sd_departments_json = {
+        "RegionIdentifier": "RI",
+        "InstitutionIdentifier": "II",
+        "Department": [
+            {
+                "ActivationDate": "1999-01-01",
+                "DeactivationDate": "2000-01-01",
+                "DepartmentIdentifier": "NY1",
+                "DepartmentLevelIdentifier": "NY1-niveau",
+                "DepartmentName": "Department 1",
+                "DepartmentUUIDIdentifier": str(SharedIdentifier.child_org_unit_uuid),
+            },
+            {
+                "ActivationDate": "1999-01-01",
+                "DeactivationDate": "2000-01-01",
+                "DepartmentIdentifier": "NY0",
+                "DepartmentLevelIdentifier": "NY0-niveau",
+                "DepartmentName": "Department 2",
+                "DepartmentUUIDIdentifier": str(SharedIdentifier.grandchild_org_unit_uuid),
+            },
+            {
+                "ActivationDate": "1999-01-01",
+                "DeactivationDate": "2000-01-01",
+                "DepartmentIdentifier": "Afd",
+                "DepartmentLevelIdentifier": "Afdelings-niveau",
+                "DepartmentName": "Department 3",
+                "DepartmentUUIDIdentifier": "30000000-0000-0000-0000-000000000000"
+            },
+            {
+                "ActivationDate": "1999-01-01",
+                "DeactivationDate": "2000-01-01",
+                "DepartmentIdentifier": "Afd",
+                "DepartmentLevelIdentifier": "Afdelings-niveau",
+                "DepartmentName": "Department 4",
+                "DepartmentUUIDIdentifier": "40000000-0000-0000-0000-000000000000"
+            },
+            {
+                "ActivationDate": "1999-01-01",
+                "DeactivationDate": "2000-01-01",
+                "DepartmentIdentifier": "NY0",
+                "DepartmentLevelIdentifier": "NY0-niveau",
+                "DepartmentName": "Department 5",
+                "DepartmentUUIDIdentifier": "50000000-0000-0000-0000-000000000000"
+            },
+            {
+                "ActivationDate": "1999-01-01",
+                "DeactivationDate": "2000-01-01",
+                "DepartmentIdentifier": "Afd",
+                "DepartmentLevelIdentifier": "Afdelings-niveau",
+                "DepartmentName": "Department 6",
+                "DepartmentUUIDIdentifier": "60000000-0000-0000-0000-000000000000"
+            },
+        ]
+    }
+    sd_departments = GetDepartmentResponse.parse_obj(sd_departments_json)
+    return sd_departments
--- sdtoolplus/tests/test_sd.py
+++ sdtoolplus/tests/test_sd.py
@@ -5,188 +5,48 @@ from uuid import UUID
 from sdclient.responses import GetDepartmentResponse
 from sdclient.responses import GetOrganizationResponse
 
-from sdtoolplus.mo_org_unit_importer import OrgUnitNode
-from sdtoolplus.sd.tree import build_tree
+from .conftest import SharedIdentifier
+from ..mo_org_unit_importer import OrgUnitNode
+from ..sd.tree import build_tree
 
 
-def test_build_tree():
-    # Arrange
-    sd_org_json = {
-        "RegionIdentifier": "RI",
-        "InstitutionIdentifier": "II",
-        "InstitutionUUIDIdentifier": "00000000-0000-0000-0000-000000000000",
-        "DepartmentStructureName": "Dep structure name",
-        "OrganizationStructure": {
-            "DepartmentLevelIdentifier": "Afdelings-niveau",
-            "DepartmentLevelReference": {
-                "DepartmentLevelIdentifier": "NY0-niveau",
-                "DepartmentLevelReference": {"DepartmentLevelIdentifier": "NY1-niveau"},
-            },
-        },
-        "Organization": [
-            {
-                "ActivationDate": "1999-01-01",
-                "DeactivationDate": "2000-01-01",
-                "DepartmentReference": [
-                    {
-                        "DepartmentIdentifier": "Afd",
-                        "DepartmentUUIDIdentifier": "30000000-0000-0000-0000-000000000000",
-                        "DepartmentLevelIdentifier": "Afdelings-niveau",
-                        "DepartmentReference": [
-                            {
-                                "DepartmentIdentifier": "NY0",
-                                "DepartmentUUIDIdentifier": "20000000-0000-0000-0000-000000000000",
-                                "DepartmentLevelIdentifier": "NY0-niveau",
-                                "DepartmentReference": [
-                                    {
-                                        "DepartmentIdentifier": "NY1",
-                                        "DepartmentUUIDIdentifier": "10000000-0000-0000-0000-000000000000",
-                                        "DepartmentLevelIdentifier": "NY1-niveau",
-                                    }
-                                ],
-                            }
-                        ],
-                    },
-                    {
-                        "DepartmentIdentifier": "Afd",
-                        "DepartmentUUIDIdentifier": "40000000-0000-0000-0000-000000000000",
-                        "DepartmentLevelIdentifier": "Afdelings-niveau",
-                        "DepartmentReference": [
-                            {
-                                "DepartmentIdentifier": "NY0",
-                                "DepartmentUUIDIdentifier": "20000000-0000-0000-0000-000000000000",
-                                "DepartmentLevelIdentifier": "NY0-niveau",
-                                "DepartmentReference": [
-                                    {
-                                        "DepartmentIdentifier": "NY1",
-                                        "DepartmentUUIDIdentifier": "10000000-0000-0000-0000-000000000000",
-                                        "DepartmentLevelIdentifier": "NY1-niveau",
-                                    }
-                                ],
-                            }
-                        ],
-                    },
-                    {
-                        "DepartmentIdentifier": "Afd",
-                        "DepartmentUUIDIdentifier": "60000000-0000-0000-0000-000000000000",
-                        "DepartmentLevelIdentifier": "Afdelings-niveau",
-                        "DepartmentReference": [
-                            {
-                                "DepartmentIdentifier": "NY0",
-                                "DepartmentUUIDIdentifier": "50000000-0000-0000-0000-000000000000",
-                                "DepartmentLevelIdentifier": "NY0-niveau",
-                                "DepartmentReference": [
-                                    {
-                                        "DepartmentIdentifier": "NY1",
-                                        "DepartmentUUIDIdentifier": "10000000-0000-0000-0000-000000000000",
-                                        "DepartmentLevelIdentifier": "NY1-niveau",
-                                    }
-                                ],
-                            }
-                        ],
-                    },
-                ],
-            }
-        ],
-    }
-
-    sd_departments_json = {
-        "RegionIdentifier": "RI",
-        "InstitutionIdentifier": "II",
-        "Department": [
-            {
-                "ActivationDate": "1999-01-01",
-                "DeactivationDate": "2000-01-01",
-                "DepartmentIdentifier": "NY1",
-                "DepartmentLevelIdentifier": "NY1-niveau",
-                "DepartmentName": "Department 1",
-                "DepartmentUUIDIdentifier": "10000000-0000-0000-0000-000000000000",
-            },
-            {
-                "ActivationDate": "1999-01-01",
-                "DeactivationDate": "2000-01-01",
-                "DepartmentIdentifier": "NY0",
-                "DepartmentLevelIdentifier": "NY0-niveau",
-                "DepartmentName": "Department 2",
-                "DepartmentUUIDIdentifier": "20000000-0000-0000-0000-000000000000",
-            },
-            {
-                "ActivationDate": "1999-01-01",
-                "DeactivationDate": "2000-01-01",
-                "DepartmentIdentifier": "Afd",
-                "DepartmentLevelIdentifier": "Afdelings-niveau",
-                "DepartmentName": "Department 3",
-                "DepartmentUUIDIdentifier": "30000000-0000-0000-0000-000000000000",
-            },
-            {
-                "ActivationDate": "1999-01-01",
-                "DeactivationDate": "2000-01-01",
-                "DepartmentIdentifier": "Afd",
-                "DepartmentLevelIdentifier": "Afdelings-niveau",
-                "DepartmentName": "Department 4",
-                "DepartmentUUIDIdentifier": "40000000-0000-0000-0000-000000000000",
-            },
-            {
-                "ActivationDate": "1999-01-01",
-                "DeactivationDate": "2000-01-01",
-                "DepartmentIdentifier": "NY0",
-                "DepartmentLevelIdentifier": "NY0-niveau",
-                "DepartmentName": "Department 5",
-                "DepartmentUUIDIdentifier": "50000000-0000-0000-0000-000000000000",
-            },
-            {
-                "ActivationDate": "1999-01-01",
-                "DeactivationDate": "2000-01-01",
-                "DepartmentIdentifier": "Afd",
-                "DepartmentLevelIdentifier": "Afdelings-niveau",
-                "DepartmentName": "Department 6",
-                "DepartmentUUIDIdentifier": "60000000-0000-0000-0000-000000000000",
-            },
-        ],
-    }
-
-    sd_departments = GetDepartmentResponse.parse_obj(sd_departments_json)
-    sd_org = GetOrganizationResponse.parse_obj(sd_org_json)
-
-    # Act
-    actual_tree = build_tree(
-        sd_org,
-        sd_departments,
-    )
-
-    # Assert
+def test_build_tree(
+    mock_sd_get_organization_response: GetOrganizationResponse,
+    mock_sd_get_department_response: GetDepartmentResponse,
+):
+    # Construct the expected tree
     expected_tree = OrgUnitNode(
-        uuid=UUID("00000000-0000-0000-0000-000000000000"),
+        uuid=SharedIdentifier.root_org_uuid,
         parent_uuid=None,
         name="<root>",
     )
     dep1 = OrgUnitNode(
-        uuid=UUID("10000000-0000-0000-0000-000000000000"),
-        parent_uuid=UUID("00000000-0000-0000-0000-000000000000"),
+        uuid=SharedIdentifier.child_org_unit_uuid,
+        parent_uuid=SharedIdentifier.root_org_uuid,
         parent=expected_tree,
         name="Department 1",
     )
     dep2 = OrgUnitNode(
-        uuid=UUID("20000000-0000-0000-0000-000000000000"),
-        parent_uuid=UUID("10000000-0000-0000-0000-000000000000"),
+        uuid=SharedIdentifier.grandchild_org_unit_uuid,
+        parent_uuid=SharedIdentifier.child_org_unit_uuid,
         parent=dep1,
         name="Department 2",
     )
     dep3 = OrgUnitNode(
         uuid=UUID("30000000-0000-0000-0000-000000000000"),
-        parent_uuid=UUID("20000000-0000-0000-0000-000000000000"),
+        parent_uuid=SharedIdentifier.grandchild_org_unit_uuid,
         parent=dep2,
         name="Department 3",
     )
     dep4 = OrgUnitNode(
         uuid=UUID("40000000-0000-0000-0000-000000000000"),
-        parent_uuid=UUID("20000000-0000-0000-0000-000000000000"),
+        parent_uuid=SharedIdentifier.grandchild_org_unit_uuid,
         parent=dep2,
         name="Department 4",
     )
     dep5 = OrgUnitNode(
         uuid=UUID("50000000-0000-0000-0000-000000000000"),
-        parent_uuid=UUID("10000000-0000-0000-0000-000000000000"),
+        parent_uuid=SharedIdentifier.child_org_unit_uuid,
         parent=dep1,
         name="Department 5",
     )
@@ -197,6 +57,13 @@ def test_build_tree():
         name="Department 6",
     )
 
+    # Act
+    actual_tree = build_tree(
+        mock_sd_get_organization_response,
+        mock_sd_get_department_response,
+    )
+
+    # Assert
     def assert_equal(node_a: OrgUnitNode, node_b: OrgUnitNode, depth: int = 0):
         assert node_a == node_b
         assert node_a.uuid == node_b.uuid
