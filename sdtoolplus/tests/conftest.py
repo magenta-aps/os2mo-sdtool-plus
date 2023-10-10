3c67dbeb6cebf8f6624fbd7777dba1106dec29c8
Mads Riis-Weitling <mrw@magenta.dk>
2023-10-10 16:18:56 +0200

    test: [#57506] Add and update `App` tests; update `TestFastAPIApp`
    This commit moves the list of expected operations from `TestOrgTreeDiff`
    to a new fixture `expected_operations`.
    
    This commit also adds a new fixture `mock_tree_diff_executor`, returning
    a mock `TreeDiffExecutor` instance.
    
    `TestApp` gains two new tests:
    * `test_execute` tests `App.execute`, checking the returned operations
    as well as the calls to `App._call_fix_departments`.
    * `test_call_fix_departments` checks the logic in
    `App._call_fix_departments`.
    
    This commit also changes `TestApp.test_get_tree_diff_executor` to focus
    on the returned `TreeDiffExecutor` instance, rather than its internal
    workings. That is supposed to be tested elsewhere.

--- sdtoolplus/tests/conftest.py
+++ sdtoolplus/tests/conftest.py
@@ -16,7 +16,10 @@ from ramodels.mo import Validity
 from sdclient.responses import GetDepartmentResponse
 from sdclient.responses import GetOrganizationResponse
 
+from ..diff_org_trees import AddOperation
 from ..diff_org_trees import OrgTreeDiff
+from ..diff_org_trees import RemoveOperation
+from ..diff_org_trees import UpdateOperation
 from ..mo_class import MOClass
 from ..mo_class import MOOrgUnitLevelMap
 from ..mo_class import MOOrgUnitTypeMap
@@ -25,7 +28,7 @@ from ..mo_org_unit_importer import OrgUnitNode
 from ..mo_org_unit_importer import OrgUnitUUID
 from ..mo_org_unit_importer import OrgUUID
 from ..sd.tree import build_tree
-
+from ..tree_diff_executor import TreeDiffExecutor
 
 _TESTING_SCHEMA_PATH = os.path.join(os.path.dirname(__file__), "mo.v7.graphql")
 _TESTING_MO_VALIDITY = Validity(from_date=datetime.now(), to_date=None)
@@ -453,3 +456,77 @@ def get_mock_sd_tree(mo_org_tree: MOOrgTreeImport) -> OrgUnitNode:
     )
     mock_sd_root.children = [mock_sd_updated_child, mock_sd_new_child]
     return mock_sd_root
+
+
+@pytest.fixture()
+def mock_tree_diff_executor(
+    mock_graphql_session: _MockGraphQLSession,
+    mock_org_tree_diff: OrgTreeDiff,
+) -> TreeDiffExecutor:
+    return TreeDiffExecutor(
+        mock_graphql_session,  # type: ignore
+        mock_org_tree_diff,
+    )
+
+
+@pytest.fixture()
+def expected_operations(
+    sd_expected_validity: Validity,
+    mock_mo_org_unit_type: MOClass,
+    mock_mo_org_unit_level_map: MockMOOrgUnitLevelMap,
+) -> list[AddOperation | UpdateOperation | RemoveOperation]:
+    return [
+        # MO unit to be removed is indeed removed
+        RemoveOperation(uuid=SharedIdentifier.removed_org_unit_uuid),
+        # MO unit "Grandchild" is renamed to "Department 2"
+        UpdateOperation(
+            uuid=SharedIdentifier.grandchild_org_unit_uuid,
+            attr="name",
+            value="Department 2",
+            validity=sd_expected_validity,
+        ),
+        # MO unit "Grandchild" has its org unit level changed to match SD
+        UpdateOperation(
+            uuid=SharedIdentifier.grandchild_org_unit_uuid,
+            attr="org_unit_level_uuid",
+            value=str(mock_mo_org_unit_level_map["NY0-niveau"].uuid),
+            validity=sd_expected_validity,
+        ),
+        # MO unit "Child" is renamed to "Department 1"
+        UpdateOperation(
+            uuid=SharedIdentifier.child_org_unit_uuid,
+            attr="name",
+            value="Department 1",
+            validity=sd_expected_validity,
+        ),
+        # MO unit "Child" has its org unit level changed to match SD
+        UpdateOperation(
+            uuid=SharedIdentifier.child_org_unit_uuid,
+            attr="org_unit_level_uuid",
+            value=str(mock_mo_org_unit_level_map["NY1-niveau"].uuid),
+            validity=sd_expected_validity,
+        ),
+        # SD units "Department 3" and "Department 4" are added under MO unit "Grandchild"
+        AddOperation(
+            parent_uuid=SharedIdentifier.grandchild_org_unit_uuid,
+            name="Department 3",
+            org_unit_type_uuid=mock_mo_org_unit_type.uuid,
+            org_unit_level_uuid=mock_mo_org_unit_level_map["Afdelings-niveau"].uuid,
+            validity=sd_expected_validity,
+        ),
+        AddOperation(
+            parent_uuid=SharedIdentifier.grandchild_org_unit_uuid,
+            name="Department 4",
+            org_unit_type_uuid=mock_mo_org_unit_type.uuid,
+            org_unit_level_uuid=mock_mo_org_unit_level_map["Afdelings-niveau"].uuid,
+            validity=sd_expected_validity,
+        ),
+        # SD unit "Department 5" is added under MO unit "Child"
+        AddOperation(
+            parent_uuid=SharedIdentifier.child_org_unit_uuid,
+            name="Department 5",
+            org_unit_type_uuid=mock_mo_org_unit_type.uuid,
+            org_unit_level_uuid=mock_mo_org_unit_level_map["NY0-niveau"].uuid,
+            validity=sd_expected_validity,
+        ),
+    ]
