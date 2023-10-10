dc38b4101154208b871bb897cb00100a97eb6078
Mads Riis-Weitling <mrw@magenta.dk>
2023-10-10 16:58:26 +0200

    chore: [#57418] Remove `__main__.py` and its tests
    This also adds some minor help on running the FastAPI server to
    `fastapi.py`.

--- sdtoolplus/fastapi.py
+++ sdtoolplus/fastapi.py
@@ -1,5 +1,14 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+"""
+To run:
+
+    $ cd os2mo-sdtool-plus
+    $ export MORA_BASE=http://localhost:5000
+    $ export AUTH_SERVER=...
+    $ export CLIENT_SECRET=...
+    $ poetry run docker/start.sh
+"""
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi import Response
