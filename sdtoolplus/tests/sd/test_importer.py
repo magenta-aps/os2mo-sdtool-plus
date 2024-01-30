98223b48e75b061931fb298ba403773ce7306297
Andreas Kring <andreas@magenta-aps.dk>
2024-01-30 13:18:34 +0100

    chore: [#59332] fixed linting

--- sdtoolplus/tests/sd/test_importer.py
+++ sdtoolplus/tests/sd/test_importer.py
@@ -15,12 +15,7 @@ def test_get_sd_departments_calls_client_with_correct_params() -> None:
     deactivation_date = date(2001, 1, 1)
 
     # Act
-    get_sd_departments(
-        mock_sd_client,
-        "II",
-        activation_date,
-        deactivation_date
-    )
+    get_sd_departments(mock_sd_client, "II", activation_date, deactivation_date)
 
     # Assert
     mock_sd_client.get_department.assert_called_once_with(
