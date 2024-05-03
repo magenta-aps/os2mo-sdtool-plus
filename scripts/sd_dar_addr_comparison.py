b522d0adfc506d5b0eebccac4b76fb8b1e29a512
Andreas Kring <andreas@magenta.dk>
2024-05-03 15:55:31 +0200

    feat: [#59332] exclude obsolete units in DAR script

--- scripts/sd_dar_addr_comparison.py
+++ scripts/sd_dar_addr_comparison.py
@@ -3,8 +3,11 @@
 from datetime import datetime
 from operator import itemgetter
 from typing import cast
+from uuid import UUID
+from uuid import uuid4
 
 import click
+from anytree.cachedsearch import find_by_attr
 from httpx import Client
 from httpx import Timeout
 from more_itertools import first
@@ -12,9 +15,21 @@ from sdclient.client import SDClient
 from sdclient.responses import Department
 
 from sdtoolplus.addresses import DARAddressUUID
+from sdtoolplus.mo_class import MOClass
+from sdtoolplus.mo_class import MOOrgUnitLevelMap
+from sdtoolplus.mo_org_unit_importer import OrgUnitNode
 from sdtoolplus.models import AddressTypeUserKey
 from sdtoolplus.sd.addresses import get_addresses
 from sdtoolplus.sd.importer import get_sd_departments
+from sdtoolplus.sd.importer import get_sd_tree
+
+
+class FakeMOOrgUnitTypeMap(MOOrgUnitLevelMap):
+    def __init__(self):
+        self.fake_uuid = uuid4()
+
+    def __getitem__(self, item: str) -> MOClass:
+        return MOClass(uuid=self.fake_uuid, user_key="not used", name="not used")
 
 
 httpx_client = Client(
@@ -38,6 +53,22 @@ def get_dar_address(dar_id: DARAddressUUID) -> str:
 
 def format_csv_field(field: str | None) -> str:
     return field if field is not None else "-"
+
+
+def _remove_if_obsolete(
+    sd_dep_with_postal_addresses: list[tuple[Department, str | None]],
+    sd_tree: OrgUnitNode,
+    obsolete_uuid: UUID,
+) -> list[tuple[Department, str | None]]:
+    non_obsolete_departments: list[tuple[Department, str | None]] = []
+    for sd_dep, addr in sd_dep_with_postal_addresses:
+        node = find_by_attr(sd_tree, sd_dep.DepartmentUUIDIdentifier, "uuid")
+        if node is None:
+            continue
+        ancestor_uuids = [ancestor.uuid for ancestor in node.ancestors]
+        if obsolete_uuid not in ancestor_uuids:
+            non_obsolete_departments.append((sd_dep, addr))
+    return non_obsolete_departments
 
 
 @click.command()
@@ -65,10 +96,18 @@ def format_csv_field(field: str | None) -> str:
     # required=True,
     help="SD institution identifier",
 )
+@click.option(
+    "--exclude-obsolete-uuid",
+    "exclude_obsolete_uuid",
+    type=click.UUID,
+    help="The UUID of the top obsolete unit ('Udgåede afdelinger'). "
+    "If set, units below this unit will be excluded",
+)
 def main(
     username: str,
     password: str,
     institution_identifier: str,
+    exclude_obsolete_uuid: UUID,
 ):
     sd_client = SDClient(username, password)
 
@@ -88,6 +127,22 @@ def main(
         for addr in get_addresses(sd_dep)
         if addr.address_type.user_key == AddressTypeUserKey.POSTAL_ADDR.value
     ]
+    print("Total number of SD units:", len(sd_dep_with_postal_addresses))
+
+    if exclude_obsolete_uuid:
+        print("Excluding obsolete units...")
+        sd_tree = get_sd_tree(
+            sd_client,
+            institution_identifier,
+            FakeMOOrgUnitTypeMap(),  # Not important when we are not writing to MO
+            build_full_tree=True,
+        )
+        sd_dep_with_postal_addresses = _remove_if_obsolete(
+            sd_dep_with_postal_addresses,
+            sd_tree,
+            exclude_obsolete_uuid,
+        )
+        print("Number of SD units after exclude:", len(sd_dep_with_postal_addresses))
 
     print("Generate CSV file")
     csv_header = (
