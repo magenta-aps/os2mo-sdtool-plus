01644ef5ea8fd05e74eb088eb7f96ba10ac3ae80
Andreas Kring <andreas@magenta.dk>
2024-05-21 13:24:14 +0200

    feat: [#60807] remove feature for excluding obsolete units

--- scripts/sd_dar_addr_comparison.py
+++ scripts/sd_dar_addr_comparison.py
@@ -4,10 +4,8 @@ from datetime import datetime
 from operator import itemgetter
 from typing import cast
 from uuid import UUID
-from uuid import uuid4
 
 import click
-from anytree.cachedsearch import find_by_attr
 from fastramqpi.raclients.graph.client import GraphQLClient
 from gql import gql
 from httpx import Client
@@ -18,13 +16,9 @@ from sdclient.client import SDClient
 from sdclient.responses import Department
 
 from sdtoolplus.addresses import DARAddressUUID
-from sdtoolplus.mo_class import MOClass
-from sdtoolplus.mo_class import MOOrgUnitLevelMap
-from sdtoolplus.mo_org_unit_importer import OrgUnitNode
 from sdtoolplus.models import AddressTypeUserKey
 from sdtoolplus.sd.addresses import get_addresses
 from sdtoolplus.sd.importer import get_sd_departments
-from sdtoolplus.sd.importer import get_sd_tree
 
 
 QUERY_GET_LINE_MANAGEMENT_CLASS = gql(
@@ -59,14 +53,6 @@ QUERY_GET_ORG_UNIT = gql(
 )
 
 
-class FakeMOOrgUnitTypeMap(MOOrgUnitLevelMap):
-    def __init__(self):
-        self.fake_uuid = uuid4()
-
-    def __getitem__(self, item: str) -> MOClass:
-        return MOClass(uuid=self.fake_uuid, user_key="not used", name="not used")
-
-
 httpx_client = Client(
     base_url="https://api.dataforsyningen.dk/",
     timeout=Timeout(timeout=30),
@@ -88,22 +74,6 @@ def get_dar_address(dar_id: DARAddressUUID) -> str:
 
 def format_csv_field(field: str | None) -> str:
     return field if field is not None else "-"
-
-
-def _remove_if_obsolete(
-    sd_dep_with_postal_addresses: list[tuple[Department, str | None]],
-    sd_tree: OrgUnitNode,
-    obsolete_uuid: UUID,
-) -> list[tuple[Department, str | None]]:
-    non_obsolete_departments: list[tuple[Department, str | None]] = []
-    for sd_dep, addr in sd_dep_with_postal_addresses:
-        node = find_by_attr(sd_tree, sd_dep.DepartmentUUIDIdentifier, "uuid")
-        if node is None:
-            continue
-        ancestor_uuids = [ancestor.uuid for ancestor in node.ancestors]
-        if obsolete_uuid not in ancestor_uuids + [node.uuid]:
-            non_obsolete_departments.append((sd_dep, addr))
-    return non_obsolete_departments
 
 
 def _get_line_management_class(gql_client: GraphQLClient) -> UUID:
@@ -157,13 +127,6 @@ def _is_line_management(
     help="SD institution identifier",
 )
 @click.option(
-    "--exclude-obsolete-uuid",
-    "exclude_obsolete_uuid",
-    type=click.UUID,
-    help="The UUID of the top obsolete unit ('Udgåede afdelinger'). "
-    "If set, units below this unit will be excluded",
-)
-@click.option(
     "--auth-server",
     "auth_server",
     type=click.STRING,
@@ -195,7 +158,6 @@ def main(
     username: str,
     password: str,
     institution_identifier: str,
-    exclude_obsolete_uuid: UUID,
     auth_server: str,
     client_id: str,
     client_secret: str,
@@ -244,21 +206,7 @@ def main(
         for sd_dep_addr in sd_dep_with_postal_addresses
         if _is_line_management(sd_dep_addr, mo_org_unit_hierarchy, line_mgmt_class)
     ]
-
-    if exclude_obsolete_uuid:
-        print("Excluding obsolete units...")
-        sd_tree = get_sd_tree(
-            sd_client,
-            institution_identifier,
-            FakeMOOrgUnitTypeMap(),  # Not important when we are not writing to MO
-            build_full_tree=True,
-        )
-        sd_dep_with_postal_addresses = _remove_if_obsolete(
-            sd_dep_with_postal_addresses,
-            sd_tree,
-            exclude_obsolete_uuid,
-        )
-        print("Number of SD units after exclude:", len(sd_dep_with_postal_addresses))
+    print("Number of SD units after filtering:", len(sd_dep_with_postal_addresses))
 
     print("Generate CSV file")
     csv_header = (
