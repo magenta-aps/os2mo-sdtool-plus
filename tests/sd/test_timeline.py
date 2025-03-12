afeccee1f39f1d46c513180b6e8314eeb2d6dcd3
Andreas Kring <andreas@magenta.dk>
2025-03-12 13:12:53 +0100

    feat: add UnitParent to SD timeline

--- tests/sd/test_timeline.py
+++ tests/sd/test_timeline.py
@@ -6,14 +6,18 @@ from unittest.mock import MagicMock
 from uuid import uuid4
 
 from more_itertools import one
+from pydantic import parse_obj_as
+from sdclient.responses import DepartmentParentHistoryObj
 from sdclient.responses import GetDepartmentResponse
 
 from sdtoolplus.models import POSITIVE_INFINITY
+from sdtoolplus.mo_org_unit_importer import OrgUnitUUID
 from sdtoolplus.models import Active
 from sdtoolplus.models import Timeline
 from sdtoolplus.models import UnitId
 from sdtoolplus.models import UnitLevel
 from sdtoolplus.models import UnitName
+from sdtoolplus.models import UnitParent
 from sdtoolplus.sd.timeline import get_department_timeline
 from sdtoolplus.sd.tree import ASSUMED_SD_TIMEZONE
 
@@ -64,9 +68,25 @@ def test_get_department_timeline():
         ],
     }
 
+    sd_parent_history_resp = [
+        {
+            "startDate": "2001-01-01",
+            "endDate": "2002-12-31",
+            "parentUuid": "8d3018f1-62ef-48cf-8e54-819b4408c23f",
+        },
+        {
+            "startDate": "2004-01-01",
+            "endDate": "9999-12-31",
+            "parentUuid": "d567f959-ab77-4849-9cd0-c14e9120433c",
+        },
+    ]
+
     mock_sd_client = MagicMock()
     mock_sd_client.get_department.return_value = GetDepartmentResponse.parse_obj(
         sd_dep_resp_dict
+    )
+    mock_sd_client.get_department_parent_history.return_value = parse_obj_as(
+        list[DepartmentParentHistoryObj], sd_parent_history_resp
     )
 
     # Act
@@ -82,6 +102,8 @@ def test_get_department_timeline():
     assert query_params.DeactivationDate == date.max
     assert query_params.DepartmentNameIndicator is True
     assert query_params.UUIDIndicator is True
+
+    mock_sd_client.get_department_parent_history.assert_called_once_with(dep_uuid)
 
     assert department_timeline.active == Timeline[Active](
         intervals=(
@@ -149,6 +171,21 @@ def test_get_department_timeline():
                 start=datetime(2005, 1, 1, tzinfo=ASSUMED_SD_TIMEZONE),
                 end=POSITIVE_INFINITY,
                 value="name4",
+            ),
+        )
+    )
+
+    assert department_timeline.parent == Timeline[UnitParent](
+        intervals=(
+            UnitParent(
+                start=datetime(2001, 1, 1, tzinfo=ASSUMED_SD_TIMEZONE),
+                end=datetime(2003, 1, 1, tzinfo=ASSUMED_SD_TIMEZONE),
+                value=OrgUnitUUID("8d3018f1-62ef-48cf-8e54-819b4408c23f"),
+            ),
+            UnitParent(
+                start=datetime(2004, 1, 1, tzinfo=ASSUMED_SD_TIMEZONE),
+                end=datetime.max.replace(tzinfo=ASSUMED_SD_TIMEZONE),
+                value=OrgUnitUUID("d567f959-ab77-4849-9cd0-c14e9120433c"),
             ),
         )
     )
