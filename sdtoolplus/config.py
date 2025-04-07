1a418e393269e608f9e5567b5e90398e3dd92a0d
Andreas Kring <andreas@magenta.dk>
2025-04-07 12:58:19 +0200

    feat: introduce municipality mode settings flag

--- sdtoolplus/config.py
+++ sdtoolplus/config.py
@@ -52,6 +52,10 @@ class SDToolPlusSettings(BaseSettings):
     sd_password: SecretStr
     sd_use_test_env: bool = False
 
+    # When true, do not prefix user_keys with the SD InstitutionIdentifier
+    # (and other stuff to come)
+    municipality_mode: bool = True
+
     # Specifies the 'user_key' of the `org_unit_type` class to use when creating new
     # org units in MO. The default value matches the existing setup at SD customers.
     org_unit_type: str = "Enhed"
