ad156fb7ac8712a7a8290105b76e59971ab79f46
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2025-06-04 21:14:03 +0200

    refactor: [#63786] refactor address handeling logic and add unittests

--- tests/test_addresses.py
+++ tests/test_addresses.py
@@ -11,6 +11,7 @@ from sdtoolplus.mo_org_unit_importer import Address
 from sdtoolplus.mo_org_unit_importer import AddressType
 from sdtoolplus.mo_org_unit_importer import OrgUnitNode
 from sdtoolplus.models import AddressTypeUserKey
+from sdtoolplus.timeline import find_address_actions
 
 
 @patch("sdtoolplus.addresses.logger")
@@ -55,3 +56,40 @@ def test_get_unit_address_logs_error_for_multiple_addr_types(
     assert mock_logger.error.call_args_list[0].kwargs["org_unit_uuid"] == str(
         org_unit_uuid
     )
+
+
+def test_find_actions_noop():
+    mo_values = {uuid4(): [1]}
+    desired_values = [1]
+    create, terminate = find_address_actions(mo_values, desired_values)
+
+    assert create == terminate == []
+
+
+def test_find_actions_new():
+    mo_values = {}
+    desired_values = [1]
+    create, terminate = find_address_actions(mo_values, desired_values)
+
+    assert create == [1]
+    assert terminate == []
+
+
+def test_find_actions_terminate():
+    address_uuid = uuid4()
+    mo_values = {address_uuid: [1]}
+    desired_values = []
+    create, terminate = find_address_actions(mo_values, desired_values)
+
+    assert create == []
+    assert terminate == [address_uuid]
+
+
+def test_find_actions_update():
+    address_uuid = uuid4()
+    mo_values = {address_uuid: [1, 2]}
+    desired_values = [2, 3]
+    create, terminate = find_address_actions(mo_values, desired_values)
+
+    assert create == [2, 3]
+    assert terminate == [address_uuid]
