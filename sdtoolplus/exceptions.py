3e2f6d31cdc1d59213470f3b76de7e4a67ecd5df
Andreas Kring <andreas@magenta.dk>
2025-06-13 16:28:01 +0200

    feat: exceptions for too few or too many objects

--- sdtoolplus/exceptions.py
+++ sdtoolplus/exceptions.py
@@ -10,6 +10,14 @@ class NoValueError(Exception):
     pass
 
 
+class ClassNotFoundError(HTTPException):
+    pass
+
+
+class MoreThanOneClassError(HTTPException):
+    pass
+
+
 class EngagementNotFoundError(HTTPException):
     pass
 
@@ -43,6 +51,35 @@ class EngagementSyncTemporarilyDisabled(HTTPException):
     pass
 
 
+class OrgUnitNotFoundError(HTTPException):
+    pass
+
+
+class MoreThanOneOrgUnitError(HTTPException):
+    pass
+
+
+class MoreThanOnePNumberError(HTTPException):
+    pass
+
+
+class MoreThanOnePostalAddressError(HTTPException):
+    pass
+
+
+class MoreThanOneLeaveError(HTTPException):
+    pass
+
+
+class_not_found_error = ClassNotFoundError(
+    status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Class not found in MO!"
+)
+
+more_than_one_class_error = MoreThanOneClassError(
+    status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+    detail="More than one class found in MO!",
+)
+
 engagement_not_found_error = EngagementNotFoundError(
     status_code=HTTP_404_NOT_FOUND,
     detail="Engagement not found for the provided person and user key (EmploymentIdentifier)",
@@ -75,4 +112,27 @@ department_timeline_not_found = DepartmentTimelineNotFound(
 engagement_sync_temporarily_disabled = EngagementSyncTemporarilyDisabled(
     status_code=HTTP_500_INTERNAL_SERVER_ERROR,
     detail="Engagement sync temporarily disabled!",
+)
+
+org_unit_not_found_error = OrgUnitNotFoundError(
+    status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Org unit not found in MO!"
+)
+
+more_than_one_org_unit_error = MoreThanOneOrgUnitError(
+    status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+    detail="More than one org unit found in MO!",
+)
+
+more_than_one_pnumber_error = MoreThanOnePNumberError(
+    status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+    detail="More than one P-number found for unit in MO!",
+)
+
+more_than_one_postal_address_error = MoreThanOnePostalAddressError(
+    status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+    detail="More than one postal address found in MO!",
+)
+
+more_than_one_leave_error = MoreThanOneLeaveError(
+    status_code=HTTP_422_UNPROCESSABLE_ENTITY, detail="More than one leave found in MO"
 )
