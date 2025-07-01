eba60bc73fe8c9b8d3f63e0f39f6518aa72a8132
Andreas Kring <andreas@magenta.dk>
2025-07-01 16:27:46 +0200

    test: initial integration test which catches OU priority sync issue

--- tests/integration/test_ou_timeline.py
+++ tests/integration/test_ou_timeline.py
@@ -412,6 +412,140 @@ async def test_ou_timeline_sd_unit_should_extend_mo_unit(
 
 
 @pytest.mark.integration_test
+async def test_ou_timeline_sd_unit_priority_sync(
+    test_client: AsyncClient,
+    graphql_client: GraphQLClient,
+    org_unit_type: OrgUnitUUID,
+    org_unit_levels: dict[str, OrgUnitLevelUUID],
+    base_tree_builder: TestingCreateOrgUnitOrgUnitCreate,
+    sd_parent_history_resp: list[dict[str, str]],
+    respx_mock: MockRouter,
+):
+    """
+    We are testing this scenario:
+
+    Time  --------t1--------t2-------------------------------t3--------------------->
+
+    MO (active)   |------------------------------------------|
+    MO (child
+        active)   |------------------------------------------|
+
+    "Arrange"     |--------------------1---------------------|
+    intervals
+
+    SD (active)             |--------------------------------|
+
+    "Assert"                |---------------1----------------|
+    intervals
+    """
+    # Arrange
+    tz = ZoneInfo("Europe/Copenhagen")
+
+    t1 = datetime(2001, 1, 1, tzinfo=tz)
+    # t2 = datetime(2002, 1, 1, tzinfo=tz)
+    t3 = datetime(2003, 1, 1, tzinfo=tz)
+
+    unit_uuid = UUID("11111111-1111-1111-1111-111111111111")
+    child_uuid = UUID("22222222-2222-2222-2222-222222222222")
+
+    # Create units (arrange interval 1)
+    await graphql_client.create_org_unit(
+        OrganisationUnitCreateInput(
+            uuid=unit_uuid,
+            validity=timeline_interval_to_mo_validity(start=t1, end=t3),
+            name="unit",
+            user_key="ABCD",
+            parent=OrgUnitUUID("10000000-0000-0000-0000-000000000000"),
+            org_unit_type=org_unit_type,
+            org_unit_level=org_unit_levels["NY1-niveau"],
+        )
+    )
+
+    await graphql_client.create_org_unit(
+        OrganisationUnitCreateInput(
+            uuid=child_uuid,
+            validity=timeline_interval_to_mo_validity(start=t1, end=t3),
+            name="child",
+            user_key="EFGH",
+            parent=unit_uuid,
+            org_unit_type=org_unit_type,
+            org_unit_level=org_unit_levels["NY0-niveau"],
+        )
+    )
+
+    sd_dep_resp = f"""<?xml version="1.0" encoding="UTF-8"?>
+        <GetDepartment20111201 creationDateTime="2025-02-18T10:41:08">
+          <RequestStructure>
+            <InstitutionIdentifier>II</InstitutionIdentifier>
+            <DepartmentUUIDIdentifier>{str(unit_uuid)}</DepartmentUUIDIdentifier>
+            <ActivationDate>1930-02-18</ActivationDate>
+            <DeactivationDate>9999-12-31</DeactivationDate>
+            <ContactInformationIndicator>false</ContactInformationIndicator>
+            <DepartmentNameIndicator>true</DepartmentNameIndicator>
+            <EmploymentDepartmentIndicator>false</EmploymentDepartmentIndicator>
+            <PostalAddressIndicator>false</PostalAddressIndicator>
+            <ProductionUnitIndicator>false</ProductionUnitIndicator>
+            <UUIDIndicator>true</UUIDIndicator>
+          </RequestStructure>
+          <RegionIdentifier>RI</RegionIdentifier>
+          <RegionUUIDIdentifier>838b8691-7785-4f64-a83a-b383567dd171</RegionUUIDIdentifier>
+          <InstitutionIdentifier>II</InstitutionIdentifier>
+          <InstitutionUUIDIdentifier>d6024493-a920-4040-9876-9faaae88efc1</InstitutionUUIDIdentifier>
+          <Department>
+            <ActivationDate>2002-01-01</ActivationDate>
+            <DeactivationDate>2002-12-31</DeactivationDate>
+            <DepartmentIdentifier>ABCD</DepartmentIdentifier>
+            <DepartmentUUIDIdentifier>{str(unit_uuid)}</DepartmentUUIDIdentifier>
+            <DepartmentLevelIdentifier>NY1-niveau</DepartmentLevelIdentifier>
+            <DepartmentName>unit</DepartmentName>
+          </Department>
+        </GetDepartment20111201>
+    """
+
+    respx_mock.get(
+        f"https://service.sd.dk/sdws/GetDepartment20111201?InstitutionIdentifier=II&DepartmentUUIDIdentifier={str(unit_uuid)}&ActivationDate=01.01.0001&DeactivationDate=31.12.9999&ContactInformationIndicator=True&DepartmentNameIndicator=True&PostalAddressIndicator=True&ProductionUnitIndicator=True&UUIDIndicator=True"
+    ).respond(
+        content_type="text/xml;charset=UTF-8",
+        content=sd_dep_resp,
+    )
+
+    respx_mock.get(
+        f"https://service.sd.dk/api-gateway/organization/public/api/v1/organizations/uuids/{str(unit_uuid)}/department-parent-history"
+    ).respond(
+        json=[
+            {
+                "startDate": "2001-01-01",
+                "endDate": "9999-12-31",
+                "parentUuid": "10000000-0000-0000-0000-000000000000",
+            },
+        ],
+    )
+
+    # Act
+    r = await test_client.post(
+        "/timeline/sync/ou",
+        json={"institution_identifier": "II", "org_unit": str(unit_uuid)},
+    )
+
+    # Assert
+    assert r.status_code == 200
+
+    # updated_unit = await graphql_client.get_org_unit_timeline(unit_uuid, None, None)
+    # validities = one(updated_unit.objects).validities
+    #
+    # assert len(validities) == 1
+    #
+    # validity = validities[0]
+    # assert validity.validity.from_ == t1
+    # assert _mo_end_to_timeline_end(validity.validity.to) == POSITIVE_INFINITY
+    # assert validity.name == "name1"
+    # assert validity.user_key == "II-ABCD"
+    # assert validity.org_unit_level is not None
+    # assert validity.org_unit_level.name == "NY0-niveau"
+    # assert validity.parent_uuid == OrgUnitUUID("10000000-0000-0000-0000-000000000000")
+
+
+@pytest.mark.integration_test
 @pytest.mark.envvar(
     {
         "MODE": "region",
