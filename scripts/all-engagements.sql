5eb0a3eb3d3f299ee3e13ddd97756d429a0ede0b
Andreas Kring <andreas@magenta-aps.dk>
2025-08-12 12:52:49 +0200

    feat: sql script for getting all engagements from MO

---
+++ scripts/all-engagements.sql
@@ -0,0 +1,26 @@
+COPY(
+    SELECT DISTINCT
+        brugervendtnoegle AS user_key,
+        rel.rel_maal_uuid AS employee,
+        brel.rel_maal_urn AS cpr
+    FROM
+        organisationfunktion_registrering reg
+    INNER JOIN
+        organisationfunktion_attr_egenskaber attr ON
+        reg.id = attr.organisationfunktion_registrering_id
+    INNER JOIN
+        organisationfunktion_relation rel ON
+        reg.id = rel.organisationfunktion_registrering_id
+    INNER JOIN
+        bruger_registrering breg ON
+        bruger_id = rel_maal_uuid
+    INNER JOIN
+        bruger_relation brel ON
+        breg.id = brel.bruger_registrering_id
+    WHERE
+        funktionsnavn = 'Engagement'
+        AND rel.rel_type = 'tilknyttedebrugere'
+        AND brel.rel_type = 'tilknyttedepersoner'
+        AND brugervendtnoegle <> '-'
+    -- LIMIT 5
+) TO '/tmp/engagements.csv' DELIMITER ',' CSV HEADER;
