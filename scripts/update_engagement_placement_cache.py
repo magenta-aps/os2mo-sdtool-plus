8a6087ac13ed0ee99e44ea7c60a8c9c132a74edf
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2025-08-12 14:26:09 +0200

    feat(RSD): [#65848] add script to update engagements json file

---
+++ scripts/update_engagement_placement_cache.py
@@ -0,0 +1,141 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+import asyncio
+import json
+from datetime import date
+from datetime import datetime
+from typing import Any
+
+import click
+import structlog
+from fastramqpi.ra_utils.asyncio_utils import gather_with_concurrency
+from sdclient.client import SDClient
+from sdclient.requests import GetEmploymentChangedAtDateRequest
+from sdclient.requests import GetEmploymentChangedRequest
+from sdclient.responses import GetEmploymentChangedAtDateResponse
+
+from sdtoolplus.apos import _engagement_timeline_to_json
+from sdtoolplus.sd.timeline import get_employment_timeline
+
+logger = structlog.stdlib.get_logger()
+
+BASE_START_DATE = datetime(1970, 1, 1)
+
+
+async def get_changed_employments(
+    sd_client: SDClient, institution_identifier: str, since: datetime
+) -> GetEmploymentChangedAtDateResponse:
+    return await asyncio.to_thread(
+        sd_client.get_employment_changed_at_date,
+        GetEmploymentChangedAtDateRequest(
+            InstitutionIdentifier=institution_identifier,
+            ActivationDate=since.date(),
+            ActivationTime=since.time(),
+            DeactivationDate=date.max,
+        ),
+    )
+
+
+async def lookup_employment_timeline(
+    sd_client: SDClient, institution_identifier: str, cpr: str, tjnrn: str
+):
+    r_employment = await asyncio.to_thread(
+        sd_client.get_employment_changed,
+        GetEmploymentChangedRequest(
+            InstitutionIdentifier=institution_identifier,
+            PersonCivilRegistrationIdentifier=cpr,
+            EmploymentIdentifier=tjnrn,
+            ActivationDate=date.min,
+            DeactivationDate=date.max,
+            DepartmentIndicator=True,
+            EmploymentStatusIndicator=True,
+            ProfessionIndicator=True,
+            WorkingTimeIndicator=True,
+            UUIDIndicator=True,
+        ),
+    )
+    sd_eng_timeline = await get_employment_timeline(r_employment)
+    eng_list = _engagement_timeline_to_json(sd_eng_timeline)
+    return eng_list
+
+
+@click.command()
+@click.option(
+    "--username",
+    "username",
+    type=click.STRING,
+    envvar="SD_USERNAME",
+    required=True,
+    help="SD username",
+)
+@click.option(
+    "--password",
+    "password",
+    type=click.STRING,
+    envvar="SD_PASSWORD",
+    required=True,
+    help="SD password",
+)
+@click.option(
+    "--institution-identifier",
+    "institution_identifier",
+    type=click.STRING,
+    envvar="SD_INSTITUTION_IDENTIFIER",
+    # required=True,
+    help="SD institution identifier",
+)
+@click.option(
+    "--file",
+    "filename",
+    type=str,
+    required=True,
+    help="File to update",
+)
+@click.option(
+    "--since",
+    type=click.DateTime(),
+    required=True,
+    help="Update file with engagements since this date",
+)
+def main(username, password, institution_identifier, filename, since: datetime):
+    logger.info("Generating engagement CSV file for the APOS importer")
+
+    with open(filename) as fp:
+        engagements: dict[str, Any] = json.load(fp)
+
+    sd_client = SDClient(username, password)
+    changed_employments = asyncio.run(
+        get_changed_employments(
+            sd_client=sd_client,
+            institution_identifier=institution_identifier,
+            since=since,
+        )
+    )
+
+    tasks = []
+    keys = []
+    for person in changed_employments.Person:
+        for eng in person.Employment:
+            keys.append(
+                f"{institution_identifier},{person.PersonCivilRegistrationIdentifier},{eng.EmploymentIdentifier}"
+            )
+            tasks.append(
+                lookup_employment_timeline(
+                    sd_client=sd_client,
+                    institution_identifier=institution_identifier,
+                    cpr=person.PersonCivilRegistrationIdentifier,
+                    tjnrn=eng.EmploymentIdentifier,
+                )
+            )
+
+    timelines = asyncio.run(gather_with_concurrency(10, *tasks))
+
+    for eng_key, timeline in zip(keys, timelines):
+        engagements["engagements"][eng_key] = timeline
+
+    with open("filename", "w") as fp:
+        json.dump(engagements, fp)
+
+
+if __name__ == "__main__":
+    main()
