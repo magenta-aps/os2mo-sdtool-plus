22e4a257469168403484f3dd4fe3cffb826ac100
Andreas Kring <andreas@magenta-aps.dk>
2025-08-25 09:12:34 +0200

    test: complete test for priority sync of child unit

--- tests/integration/test_ou_timeline.py
+++ tests/integration/test_ou_timeline.py
@@ -7,6 +7,7 @@ from uuid import uuid4
 from zoneinfo import ZoneInfo
 
 import pytest
+from fastramqpi.pytest_util import retry
 from httpx import AsyncClient
 from more_itertools import one
 from respx import MockRouter
@@ -412,8 +413,15 @@ async def test_ou_timeline_sd_unit_should_extend_mo_unit(
     assert validity.parent_uuid == OrgUnitUUID("10000000-0000-0000-0000-000000000000")
 
 
-@unittest.skip("To be completed later")
 @pytest.mark.integration_test
+@pytest.mark.envvar(
+    {
+        "MODE": "region",
+        "UNKNOWN_UNIT": str(UNKNOWN_UNIT),
+        "APPLY_NY_LOGIC": "false",
+        "MO_SUBTREE_PATHS_FOR_ROOT": '{"II": ["12121212-1212-1212-1212-121212121212", "10000000-0000-0000-0000-000000000000"]}',
+    }
+)
 async def test_ou_timeline_sd_unit_priority_sync(
     test_client: AsyncClient,
     graphql_client: GraphQLClient,
@@ -444,7 +452,7 @@ async def test_ou_timeline_sd_unit_priority_sync(
     tz = ZoneInfo("Europe/Copenhagen")
 
     t1 = datetime(2001, 1, 1, tzinfo=tz)
-    # t2 = datetime(2002, 1, 1, tzinfo=tz)
+    t2 = datetime(2002, 1, 1, tzinfo=tz)
     t3 = datetime(2003, 1, 1, tzinfo=tz)
 
     unit_uuid = UUID("11111111-1111-1111-1111-111111111111")
@@ -504,11 +512,47 @@ async def test_ou_timeline_sd_unit_priority_sync(
         </GetDepartment20111201>
     """
 
+    sd_dep_resp_child = f"""<?xml version="1.0" encoding="UTF-8"?>
+        <GetDepartment20111201 creationDateTime="2025-02-18T10:41:08">
+          <RequestStructure>
+            <InstitutionIdentifier>II</InstitutionIdentifier>
+            <DepartmentUUIDIdentifier>{str(child_uuid)}</DepartmentUUIDIdentifier>
+            <ActivationDate>1930-02-18</ActivationDate>
+            <DeactivationDate>9999-12-31</DeactivationDate>
+            <ContactInformationIndicator>false</ContactInformationIndicator>
+            <DepartmentNameIndicator>true</DepartmentNameIndicator>
+            <EmploymentDepartmentIndicator>false</EmploymentDepartmentIndicator>
+            <PostalAddressIndicator>false</PostalAddressIndicator>
+            <ProductionUnitIndicator>false</ProductionUnitIndicator>
+            <UUIDIndicator>true</UUIDIndicator>
+          </RequestStructure>
+          <RegionIdentifier>RI</RegionIdentifier>
+          <RegionUUIDIdentifier>838b8691-7785-4f64-a83a-b383567dd171</RegionUUIDIdentifier>
+          <InstitutionIdentifier>II</InstitutionIdentifier>
+          <InstitutionUUIDIdentifier>d6024493-a920-4040-9876-9faaae88efc1</InstitutionUUIDIdentifier>
+          <Department>
+            <ActivationDate>2002-01-01</ActivationDate>
+            <DeactivationDate>2002-12-31</DeactivationDate>
+            <DepartmentIdentifier>EFGH</DepartmentIdentifier>
+            <DepartmentUUIDIdentifier>{str(child_uuid)}</DepartmentUUIDIdentifier>
+            <DepartmentLevelIdentifier>NY0-niveau</DepartmentLevelIdentifier>
+            <DepartmentName>unit</DepartmentName>
+          </Department>
+        </GetDepartment20111201>
+    """
+
     respx_mock.get(
         f"https://service.sd.dk/sdws/GetDepartment20111201?InstitutionIdentifier=II&DepartmentUUIDIdentifier={str(unit_uuid)}&ActivationDate=01.01.0001&DeactivationDate=31.12.9999&ContactInformationIndicator=True&DepartmentNameIndicator=True&PostalAddressIndicator=True&ProductionUnitIndicator=True&UUIDIndicator=True"
     ).respond(
         content_type="text/xml;charset=UTF-8",
         content=sd_dep_resp,
+    )
+
+    respx_mock.get(
+        f"https://service.sd.dk/sdws/GetDepartment20111201?InstitutionIdentifier=II&DepartmentUUIDIdentifier={str(child_uuid)}&ActivationDate=01.01.0001&DeactivationDate=31.12.9999&ContactInformationIndicator=True&DepartmentNameIndicator=True&PostalAddressIndicator=True&ProductionUnitIndicator=True&UUIDIndicator=True"
+    ).respond(
+        content_type="text/xml;charset=UTF-8",
+        content=sd_dep_resp_child,
     )
 
     respx_mock.get(
@@ -523,6 +567,18 @@ async def test_ou_timeline_sd_unit_priority_sync(
         ],
     )
 
+    respx_mock.get(
+        f"https://service.sd.dk/api-gateway/organization/public/api/v1/organizations/uuids/{str(child_uuid)}/department-parent-history"
+    ).respond(
+        json=[
+            {
+                "startDate": "2002-01-01",
+                "endDate": "2002-12-31",
+                "parentUuid": str(unit_uuid),
+            },
+        ],
+    )
+
     # Act
     r = await test_client.post(
         "/timeline/sync/ou",
@@ -530,21 +586,34 @@ async def test_ou_timeline_sd_unit_priority_sync(
     )
 
     # Assert
-    assert r.status_code == 200
+    assert r.status_code == 500
 
-    # updated_unit = await graphql_client.get_org_unit_timeline(unit_uuid, None, None)
-    # validities = one(updated_unit.objects).validities
-    #
-    # assert len(validities) == 1
-    #
-    # validity = validities[0]
-    # assert validity.validity.from_ == t1
-    # assert _mo_end_to_timeline_end(validity.validity.to) == POSITIVE_INFINITY
-    # assert validity.name == "name1"
-    # assert validity.user_key == "II-ABCD"
-    # assert validity.org_unit_level is not None
-    # assert validity.org_unit_level.name == "NY0-niveau"
-    # assert validity.parent_uuid == OrgUnitUUID("10000000-0000-0000-0000-000000000000")
+    @retry()
+    async def verify() -> None:
+        # Check the unit itself
+        updated_unit = await graphql_client.get_org_unit_timeline(
+            unit_uuid=unit_uuid,
+            from_date=None,
+            to_date=None,
+        )
+
+        validity = one(one(updated_unit.objects).validities)
+
+        assert validity.validity.from_ == t2
+        assert _mo_end_to_timeline_end(validity.validity.to) == t3
+
+        # Check the child unit
+        updated_unit = await graphql_client.get_org_unit_timeline(
+            unit_uuid=child_uuid,
+            from_date=None,
+            to_date=None,
+        )
+
+        validity = one(one(updated_unit.objects).validities)
+
+        assert validity.validity.from_ == t2
+        assert _mo_end_to_timeline_end(validity.validity.to) == t3
+        assert validity.parent_uuid == unit_uuid
 
 
 @unittest.skip("To be completed later")
