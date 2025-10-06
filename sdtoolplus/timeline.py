bd18676a22cda07585d618200f0b81ba96b0c748
Andreas Kring <andreas@magenta-aps.dk>
2025-10-06 11:19:57 +0200

    refactor: move MO leave functions to separate module

--- sdtoolplus/timeline.py
+++ sdtoolplus/timeline.py
@@ -36,20 +36,15 @@ from sdtoolplus.exceptions import MoreThanOneEngagementError
 from sdtoolplus.exceptions import MoreThanOnePersonError
 from sdtoolplus.exceptions import NoValueError
 from sdtoolplus.mo.timeline import create_association
-from sdtoolplus.mo.timeline import create_leave
 from sdtoolplus.mo.timeline import create_person
 from sdtoolplus.mo.timeline import get_association_filter
 from sdtoolplus.mo.timeline import (
     get_association_timeline as get_mo_association_timeline,
 )
 from sdtoolplus.mo.timeline import get_class
-from sdtoolplus.mo.timeline import get_leave_timeline as get_mo_leave_timeline
 from sdtoolplus.mo.timeline import related_units
 from sdtoolplus.mo.timeline import terminate_association
-from sdtoolplus.mo.timeline import terminate_leave
-from sdtoolplus.mo.timeline import terminate_leave_before_engagement_termination
 from sdtoolplus.mo.timeline import update_association
-from sdtoolplus.mo.timeline import update_leave
 from sdtoolplus.mo.timeline import update_person
 from sdtoolplus.mo.timelines.engagement import create_engagement
 from sdtoolplus.mo.timelines.engagement import get_engagement_filter
@@ -58,6 +53,11 @@ from sdtoolplus.mo.timelines.engagement import get_engagement_types
 from sdtoolplus.mo.timelines.engagement import get_job_function
 from sdtoolplus.mo.timelines.engagement import terminate_engagement
 from sdtoolplus.mo.timelines.engagement import update_engagement
+from sdtoolplus.mo.timelines.leave import create_leave
+from sdtoolplus.mo.timelines.leave import get_leave_timeline as get_mo_leave_timeline
+from sdtoolplus.mo.timelines.leave import terminate_leave
+from sdtoolplus.mo.timelines.leave import terminate_leave_before_engagement_termination
+from sdtoolplus.mo.timelines.leave import update_leave
 from sdtoolplus.mo.timelines.org_unit import create_ou
 from sdtoolplus.mo.timelines.org_unit import create_phone_number
 from sdtoolplus.mo.timelines.org_unit import create_pnumber_address
