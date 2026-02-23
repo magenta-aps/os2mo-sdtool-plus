b0b9bdbc405e771e05ca9382a45515a621d5bc20
Andreas Kring <andreas@magenta-aps.dk>
2026-02-23 10:32:55 +0100

    test: add validator unit test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,11 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+from datetime import datetime
+from datetime import timezone
+
 import pytest
 
+from sdtoolplus.config import TIMEZONE
 from sdtoolplus.config import SDToolPlusSettings
 
 
@@ -10,11 +14,27 @@ def test_unknown_unit_is_set_when_municipality_mode_is_false(
 ):
     # Arrange
     settings = sdtoolplus_settings.dict()
-    settings_dict = settings.update({"municipality_mode": False})
+    settings.update({"municipality_mode": False})
 
     # Act + Assert
     with pytest.raises(ValueError) as error:
-        SDToolPlusSettings.parse_obj(settings_dict)
+        SDToolPlusSettings.parse_obj(settings)
         assert error == ValueError(
             "Unknown unit must be set when MUNICIPALITY_MODE is false"
         )
+
+
+def test_min_mo_datetime_validator(sdtoolplus_settings: SDToolPlusSettings):
+    # Arrange
+    settings = sdtoolplus_settings.dict()
+    settings.update(
+        {"min_mo_datetime": datetime(1970, 1, 1, 11, 0, 0, tzinfo=timezone.utc)}
+    )
+
+    # Act
+    validated_settings = SDToolPlusSettings.parse_obj(settings)
+
+    # Assert
+    assert validated_settings.min_mo_datetime == datetime(
+        1970, 1, 1, 12, 0, 0, tzinfo=TIMEZONE
+    )
